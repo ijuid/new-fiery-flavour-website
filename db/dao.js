398faa2727564914aa1861c41164774c6209fb33
n1colaa <nicolaayedogbon12@gmail.com>
2025-11-16 22:32:38 +0000

    started creating table

--- db/dao.js
+++ db/dao.js
@@ -1,5 +1,6 @@
 const mysql = require("mysql");
-
+var express = require("express");
+var router = express.Router();
 var pool = mysql.createPool({
   connectionLimit: 100,
   host: "sql8.freesqldatabase.com",
@@ -25,3 +26,23 @@ function queryDatabase(query, callback) {
     }
   });
 }
+
+con.connect(function(err){
+  if (err) throw err;
+  console.log("Connected");
+  let sql = "CREATE TABLE reservation (name VARCHAR(255), partySize INT(20), bookingDate DATE, bookingTime )
+})
+
+router.post("/booking.html", function (req, res, next) {
+    console.log(req.fname);
+    console.log(req.size);
+    console.log(req.date);
+    console.log(req.time);
+    console.log(req.contactNum);
+    console.log(req.email);
+    con.connect(function (err) {
+      if (err) throw err;
+      console.log("Connected");
+      var sql = Insert;
+    });
+  });
